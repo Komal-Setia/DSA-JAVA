0876231b1d758977268a789b029f4697dd37b89f
Komal <komal0527.cse19@chitkara.edu.in>
2025-02-03 22:24:02 +0530

    IntroductionToStacks

--- dsa/src/stacks/IntroductionToStacks.java
+++ dsa/src/stacks/IntroductionToStacks.java
@@ -1,9 +1,34 @@
 package stacks;
-
+import java.util.*;
 public class IntroductionToStacks {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Stack<Integer> st = new Stack<>();
+
+		st.push(10);//Add elements in stack
+		System.out.println("Stack ->" + st);
+		if(!st.empty()) {
+			System.out.println("Top Element "+ st.peek()); //Top element
+		}
+		st.push(20);
+		System.out.println("Stack ->" + st);
+		st.push(30);
+		System.out.println("Stack ->" + st);
+		st.push(40);
+		System.out.println("Stack ->" + st);
+		st.push(50);
+		System.out.println("Stack ->" + st);
+		if(!st.empty()) {
+			System.out.println("Top Element "+ st.peek()); //Top element
+		}
+		if(!st.empty()) {
+			System.out.println("Removed Element "+ st.pop()); //Remove Element
+		}
+		System.out.println("Stack ->" + st);
+		if(!st.empty()) {
+			System.out.println("Top Element "+ st.peek()); //Top element
+		}
+		System.out.println("Stack ->" + st);
 
 	}
 
